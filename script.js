64813ee096a13e2e402cb72e111ab08502b2caad
gabriel <gabrielgleste@gmail.com>
2024-04-01 09:01:32 -0300

    criando o script do calculo

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-function calc{
-    let num1=(document.getElementById(num1).value);
+function calc(){
+    let num1=parseInt(document.getElementById('num1').value);
+    let num2=parseInt(document.getElementById('num2').value);
+    document.getElementById('resultado').innerHTML=num1 * num2
 }
